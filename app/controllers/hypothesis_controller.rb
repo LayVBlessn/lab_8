5ac957d31804f6606a43951608617ce968961bdf
LayV <myakinb@mail.ru>
2021-12-23 03:18:43 +0300

    first commit

---
+++ app/controllers/hypothesis_controller.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class HypothesisController < ApplicationController
+  def index; end
+
+  def create
+    @iterations = proccessor(params[:number])
+  end
+
+  private
+
+  def proccessor(number)
+    iterations = []
+    while number.to_s != number.to_s.reverse
+      number = number.to_i + number.to_s.reverse.to_i
+      iterations << number
+    end
+    iterations
+  end
+end
